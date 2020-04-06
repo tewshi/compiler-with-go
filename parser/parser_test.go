49fe88c02ef540dec2f7bdea0b3eb4267892a8eb
Joseph Michael <tjoe4u@gmail.com>
2020-04-06 15:24:44 +0100

    add tests that fail to show errors

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -40,6 +40,40 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func TestWrongLetStatements(t *testing.T) {
+	input := `
+	let x 5;
+	let = 10;
+	let 838383;
+	`
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
 func checkParserErrors(t *testing.T, p *Parser) {
 	errors := p.Errors()
 
