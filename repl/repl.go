e65d89963bd09c85978f7ef71d35a5236bfae74a
Joseph Michael <tjoe4u@gmail.com>
2020-04-10 14:48:59 +0100

    add some flesh to the rlpl error log

--- repl/repl.go
+++ repl/repl.go
@@ -11,6 +11,34 @@ import (
 // PROMPT the text that shows on the console
 const PROMPT = ">> "
 
+// MONKEYSMILE monkey face meme
+const MONKEYSMILE = `         __,__
+   .--. .-" "-. .--.
+/ .. \/ .-. .-. \/ .. \
+| | | '| / Y \ |' | | |
+|| \ \ \ 0 | 0 / / / ||
+ \ '- ,\.-"""-./, -' /
+  ''-' /_ ^ ^ _\ '-''
+      | \._ _./ |
+      \ \ '~' / /
+      '._'-=-'_.'
+        '-----'
+`
+
+// MONKEYFROWN monkey face meme
+const MONKEYFROWN = `         __,__
+   .--. .-" "-. .--.
+/ .. \/ .-. .-. \/ .. \
+| | | '| / Y \ |' | | |
+|| \ \ \ * | * / / / ||
+ \ '- ,\.-"""-./, -' /
+  ''-' /_ ^ ^ _\ '-''
+      \ \.~~~./ /
+      | \.~~~./ |
+      '._'-=-'_.'
+        '-----'
+`
+
 // Start is the main function that starts this repl
 func Start(in io.Reader, out io.Writer) {
 	// create the scanner
@@ -47,6 +75,9 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEYFROWN)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
