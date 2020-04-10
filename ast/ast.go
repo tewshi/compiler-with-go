b1a4477f314ee8ba79902f357c6361f3202f4c0e
Joseph Michael <tjoe4u@gmail.com>
2020-04-10 13:45:47 +0100

    add function call expression struct

--- ast/ast.go
+++ ast/ast.go
@@ -288,3 +288,29 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString(fl.Body.String())
 	return out.String()
 }
+
+// CallExpression represents a function call expression
+type CallExpression struct {
+	Token     token.Token // The '(' token
+	Function  Expression  // Identifier or FunctionLiteral
+	Arguments Expressions
+}
+
+func (ce *CallExpression) expressionNode() {}
+
+// TokenLiteral the literal value of the function call token
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+
+// String string representation of a function call literal
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+	return out.String()
+}
