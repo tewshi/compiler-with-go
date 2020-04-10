127db2475f79a05939f6ca19c96b9b923f09e72a
Joseph Michael <tjoe4u@gmail.com>
2020-04-10 14:09:38 +0100

    add test for return statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -39,6 +39,41 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func TestLetStatementsWithReturn(t *testing.T) {
+	tests := []struct {
+		input              string
+		expectedIdentifier string
+		expectedValue      interface{}
+	}{
+		{"let x = 5;", "x", 5},
+		{"let y = true;", "y", true},
+		{"let foobar = y;", "foobar", "y"},
+	}
+
+	for _, tt := range tests {
+		l := lexer.NewLexer(tt.input)
+		p := NewParser(l)
+		program := p.ParseProgram()
+
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+				len(program.Statements))
+		}
+
+		stmt := program.Statements[0]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+
+		val := stmt.(*ast.LetStatement).Value
+		if !testLiteralExpression(t, val, tt.expectedValue) {
+			return
+		}
+	}
+}
+
 func TestWrongLetStatements(t *testing.T) {
 	t.Skip("skipping failing wrong let statement test")
 	input := `
