f4152590e42d8a587deb3ed9287184fca479dec6
Joseph Michael <tjoe4u@gmail.com>
2020-04-11 09:01:50 +0100

    add eval functions for prefix and infix operators

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -3,6 +3,21 @@ package evaluator
 import (
 	"monkey/ast"
 	"monkey/object"
+	"monkey/token"
+)
+
+var (
+	// NULL holds a single null value for reuse
+	NULL = &object.Null{}
+
+	// NAN holds a single nan value for reuse
+	NAN = &object.Nan{}
+
+	// TRUE holds a single true value for reuse
+	TRUE = &object.Boolean{Value: true}
+
+	// FALSE holds a single false value for reuse
+	FALSE = &object.Boolean{Value: false}
 )
 
 // Eval returns the evaluated node as an object
@@ -13,11 +28,18 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
-	// Expressions
+		// Expressions
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -28,4 +50,107 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	switch operator {
+	case token.PLUS:
+		return evalPlusOperatorExpression(left, right)
+	case token.MINUS:
+		return evalSubtractOperatorExpression(left, right)
+	case token.ASTERISK:
+		return evalMultiplyOperatorExpression(left, right)
+	case token.SLASH:
+		return evalDivideOperatorExpression(left, right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalPlusOperatorExpression(left object.Object, right object.Object) object.Object {
+	if left.Type() != object.INTEGEROBJ || right.Type() != object.INTEGEROBJ {
+		return NULL
+	}
+
+	lvalue := left.(*object.Integer).Value
+	rvalue := right.(*object.Integer).Value
+
+	return &object.Integer{Value: lvalue + rvalue}
+}
+
+func evalSubtractOperatorExpression(left object.Object, right object.Object) object.Object {
+	if left.Type() != object.INTEGEROBJ || right.Type() != object.INTEGEROBJ {
+		return NULL
+	}
+
+	lvalue := left.(*object.Integer).Value
+	rvalue := right.(*object.Integer).Value
+
+	return &object.Integer{Value: lvalue - rvalue}
+}
+
+func evalMultiplyOperatorExpression(left object.Object, right object.Object) object.Object {
+	if left.Type() != object.INTEGEROBJ || right.Type() != object.INTEGEROBJ {
+		return NULL
+	}
+
+	lvalue := left.(*object.Integer).Value
+	rvalue := right.(*object.Integer).Value
+
+	return &object.Integer{Value: lvalue * rvalue}
+}
+
+func evalDivideOperatorExpression(left object.Object, right object.Object) object.Object {
+	if left.Type() != object.INTEGEROBJ || right.Type() != object.INTEGEROBJ {
+		return NULL
+	}
+
+	lvalue := left.(*object.Integer).Value
+	rvalue := right.(*object.Integer).Value
+
+	if rvalue == 0 {
+		return NAN
+	}
+
+	return &object.Integer{Value: lvalue / rvalue}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGEROBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+
+	return &object.Integer{Value: -value}
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
