f5c74734268497c44fb31c7d8fa59811d808885c
Joseph Michael <tjoe4u@gmail.com>
2020-04-11 09:02:03 +0100

    use evaluator in repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -68,9 +69,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		// print our program string
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		// print our evaluated program
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
