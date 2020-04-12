8a6129b88b3dd21b179567d33138b5554fefdf41
Joseph Michael <tjoe4u@gmail.com>
2020-04-12 03:14:55 +0100

    add test for function call

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -131,6 +131,9 @@ func TestReturnStatements(t *testing.T) {
 			
 			return 1;
 		}`, 10},
+
+		{"let addTwo = fn(x) { x + 2; }; addTwo(2);", 4},
+		{"let multiply = fn(x, y) { x * y }; multiply(50 / 2, 1 * 2);", 50},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -233,6 +236,23 @@ func TestFunctionObject(t *testing.T) {
 	expectedBody := "(x + 2)"
 	if fn.Body.String() != expectedBody {
 		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
 
