0306f89512f6d968c5c69b5ef5142f5be4e28bf4
Joseph Michael <tjoe4u@gmail.com>
2020-04-12 03:14:23 +0100

    extend environment to allow saving the outer scope

--- object/environment.go
+++ object/environment.go
@@ -3,6 +3,7 @@ package object
 // Environment the environment struct
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 // NewEnvironment creates and returns a new environment
@@ -11,9 +12,21 @@ func NewEnvironment() *Environment {
 	return &Environment{store: s}
 }
 
+// NewEnclosedEnvironment creates and returns a new enclosed environment
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+
+	return env
+}
+
 // Get returns value associated with the given environment key (name)
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	// attempt to load from enclosing environment
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
