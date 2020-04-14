5686f172a93a9bc9a07de8c221113d19293be832
Joseph Michael <tjoe4u@gmail.com>
2020-04-14 17:31:03 +0100

    add util func for getting float precision

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"strings"
+)
+
 // InArray returns true if the needle exists in the haystack
 func InArray(needle interface{}, haystack []interface{}) bool {
 	for _, item := range haystack {
@@ -17,4 +21,14 @@ func Abs(a int64) int64 {
 	}
 
 	return a
+}
+
+// Precision returns the precision of a double
+func Precision(s string) int {
+	parts := strings.Split(s, ".")
+	if len(parts) != 2 {
+		return 0
+	}
+
+	return len(parts[1])
 }
