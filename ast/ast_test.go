4e5c5b6d5437a8d8b2877fb9d41871f9d60bfe45
Joseph Michael <tjoe4u@gmail.com>
2020-04-17 19:48:35 +0100

    add more tests to AST

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -23,10 +23,36 @@ func TestString(t *testing.T) {
 				Token: token.Token{Type: token.COMMENT, Literal: "// this is a comment"},
 				Value: "this is a comment",
 			},
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "x"},
+					Value: "x",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+			},
+			&ExpressionStatement{
+				Expression: &InfixExpression{
+					Token:    token.Token{Type: token.PLUSEQ, Literal: token.PLUSEQ},
+					Left:     &Identifier{Token: token.Token{Type: token.IDENT, Literal: "x"}, Value: "x"},
+					Operator: token.PLUSEQ,
+					Right:    &Identifier{Token: token.Token{Type: token.IDENT, Literal: "x"}, Value: "x"},
+				},
+			},
+			&ExpressionStatement{
+				Expression: &SuffixExpression{
+					Token:    token.Token{Type: token.INCREMENT, Literal: token.INCREMENT},
+					Left:     &Identifier{Token: token.Token{Type: token.IDENT, Literal: "x"}, Value: "x"},
+					Operator: token.INCREMENT,
+				},
+			},
 		},
 	}
 
-	if program.String() != "let myVar = anotherVar;// this is a comment" {
+	if program.String() != "let myVar = anotherVar;let x = myVar;(x += x)(x++)" {
 		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
